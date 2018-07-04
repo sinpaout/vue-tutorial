458002492c57ffba794e848f4b8c0233315329b9
Thet Win Aung <minthit21dj@gmail.com>
2018-07-05 00:04:24 +0900

    Add vuex plugin and logger

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createLogger from 'vuex/dist/logger'
 import App from './App.vue'
 // import App from './components/HelloWorld.vue'
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
+
+const plugin = store => {
+  store.subscribe(mutation => {
+    // console.log('mutation', mutation);
+    if (mutation.type === 'recievedData') {
+    }
+  })
+}
 
 const store = new Vuex.Store({
   state: {
@@ -13,7 +22,8 @@ const store = new Vuex.Store({
   mutations: {
     increment: state => state.count++,
     decrement: state => state.count--
-  }
+  },
+  plugins: [plugin, createLogger()]
 })
 
 window.store = store
